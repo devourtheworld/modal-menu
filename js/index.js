90d80402159c5e0db4b02ccd9235ea44fb55f702
zebestlove <zbyslav13@gmail.com>
2020-08-10 19:58:07 +0300

    new sidenav

--- js/index.js
+++ js/index.js
@@ -1,37 +1,9 @@
-const toggle = document.getElementById("toggle");
-const navbar = document.getElementById("navbar");
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
 
-// This function closes navbar if user clicks anywhere outside of navbar once it's opened
-// Does not leave unused event listeners on
-// It's messy, but it works
-// function closeNavbar(e) {
-//   if (
-//     document.body.classList.contains("show-nav") &&
-//     e.target !== toggle &&
-//     !toggle.contains(e.target) &&
-//     e.target !== navbar &&
-//     !navbar.contains(e.target)
-//   ) {
-//     document.body.classList.toggle("show-nav");
-//     document.body.removeEventListener("click", closeNavbar);
-//   } else if (!document.body.classList.contains("show-nav")) {
-//     document.body.removeEventListener("click", closeNavbar);
-//   }
-// }
-
-// Toggle nav
-toggle.addEventListener("click", () => {
-  document.body.classList.toggle("show-nav");
-  document.body.addEventListener("click", closeNavbar);
-});
-
-// Show modal
-// open.addEventListener("click", () => modal.classList.add("show-modal"));
-
-// // Hide modal
-// close.addEventListener("click", () => modal.classList.remove("show-modal"));
-
-// Hide modal on outside click
-// window.addEventListener("click", (e) =>
-//   e.target == modal ? modal.classList.remove("show-modal") : false
-// );
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
